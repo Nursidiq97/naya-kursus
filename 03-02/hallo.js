f753122c31ec9db3e9ff3078f53230a7d660da58
Nursidiq97 <sidiqnur363@gmail.com>
2023-02-01 14:44:55 +0700

    Uppluad-naya-kursus

---
+++ 03-02/hallo.js
@@ -0,0 +1,25 @@
+var v = " Name : Nur Sidiq";
+var x = " Umur :20 Tahun";
+var y = " Jenis Kelamin : laki-laki";
+
+console.log(v,x,y);
+
+let w = 2;
+let l = 3;
+s = 6;
+
+console.log(s);
+
+//let= tidak bisa di panggil nama yang sama
+
+const p = 4;
+const b = 6;
+const c = 4;
+
+console.log(c);
+
+const m = "Hallo Java Script";
+console.log(typeof m);
+
+const r = true;
+console.log(typeof r);
---
+++ 03-02/index.js
@@ -0,0 +1,10 @@
+var x = 1;
+console.log(x);
+console.log(1278896);
+
+x2 = 2;
+console.log(x);
+
+const jmlBuku = 2;
+const y = 2;
+console.log(y);
